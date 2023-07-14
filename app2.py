89f8928c7641640b77fcde26d699821b31e201ed
piotek8 <piotr.bator.contact@gmai.com>
2023-07-14 17:47:03 +0200

    create agent in langchain with math and llm functions

---
+++ app2.py
@@ -0,0 +1,58 @@
+from langchain.chat_models import ChatOpenAI
+from langchain.chains import LLMMathChain, LLMChain
+from langchain.agents import Tool
+from langchain.agents import initialize_agent
+from langchain.prompts import PromptTemplate
+import os
+from dotenv import load_dotenv, find_dotenv
+
+
+class Agent:
+    def __init__(self):
+        load_dotenv(find_dotenv())
+        self.llm = ChatOpenAI(
+            openai_api_key=os.getenv("OPENAI_API_KEY"),
+            temperature=0,
+            model_name="gpt-3.5-turbo-16k"
+        )
+
+        # initialize the math tool
+        self.llm_math = LLMMathChain(llm=self.llm)
+        self.math_tool = Tool(
+            name='Calculator',
+            func=self.llm_math.run,
+            description='Useful for when you need to answer questions about math.'
+        )
+
+        # when giving tools to LLM, we must pass as a list of tools
+        self.tools = [self.math_tool]
+
+        # initialize the LLM tool
+        self.llm_chain = LLMChain(llm=self.llm)
+        self.llm_tool = Tool(
+            name='Language Model',
+            func=self.llm_chain.run,
+            description='Use this tool for general purpose queries and logic.'
+        )
+        self.tools.append(self.llm_tool)
+
+        # initialize the zero-shot agent
+        self.zero_shot_agent = initialize_agent(
+            agent="zero-shot-react-description",
+            tools=self.tools,
+            llm=self.llm,
+            verbose=True,
+            max_iterations=3
+        )
+
+    def run_agent(self):
+        response = input('''Write information related to mathematics only\nEnter your question: ''')
+        result = self.zero_shot_agent(response)
+        print(result)
+
+
+# create an instance of Agent
+agent = Agent()
+
+# run the agent
+agent.run_agent()
