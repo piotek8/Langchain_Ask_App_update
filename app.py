1061336346cef1ddb1769d2fc365dfb9179e55a5
piotek8 <piotr.bator.contact@gmai.com>
2023-07-15 12:27:19 +0200

    create agent in langchain with math and llm functions

--- app.py
+++ app.py
@@ -5,10 +5,34 @@ if __name__ == "__main__":
     agent = Agent()
 
     # run the agent
-    question = input('''Write information related to mathematics only\nEnter your question: ''')
+    question = input('''Write informations \nEnter your question: ''')
     agent.get_agent_response(question)
 
     # what is (6.53*14.1)^6.22?
     # "if Mary has four apples and Giorgio brings two and a half apple "
     #               "boxes (apple box contains eight apples), how many apples do we "
     #               "have?")
+#The famr consists of a house,
+# a shack and a barn. How many dogs are on the farm?
+# If 2 dogs are in the kennel and 2 other dogs are standing next to the house.
+
+
+
+
+'''
+from langchain.prompts import PromptTemplate
+from langchain.chains import LLMChain
+
+prompt = PromptTemplate(
+    input_variables=["query"],
+    template="{query}"
+)
+
+llm_chain = LLMChain(llm=llm, prompt=prompt)
+
+# initialize the LLM tool
+llm_tool = Tool(
+    name='Language Model',
+    func=llm_chain.run,
+    description='use this tool for general purpose queries and logic'
+'''
--- core/agent.py
+++ core/agent.py
@@ -3,17 +3,19 @@ import os
 from dotenv import load_dotenv, find_dotenv
 from langchain.agents import Tool
 from langchain.agents import initialize_agent
-from langchain.chains import LLMMathChain
+from langchain.chains import LLMMathChain, LLMChain
 from langchain.chat_models import ChatOpenAI
+from langchain.prompts import PromptTemplate
 
-from utils.constants import OPENAI_KEY
+#from utils.constants import OPENAI_KEY
 
 
 class Agent:
     def __init__(self):
         load_dotenv(find_dotenv())
         self.llm = ChatOpenAI(
-            openai_api_key=os.getenv(OPENAI_KEY),
+            openai_api_key=os.getenv("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"),
+            #(OPENAI_KEY),
             temperature=0,
             model_name="gpt-3.5-turbo-16k"
         )
@@ -29,6 +31,30 @@ class Agent:
         # when giving tools to LLM, we must pass as a list of tools
         self.tools = [self.math_tool]
 
+    #    # initialize the LLM tool
+    #    self.llm_chain = LLMChain(llm=self.llm)
+    #    self.llm_tool = Tool(
+    #        name='Language Model',
+    #        func=self.llm_chain.run,
+    #        description='Use this tool for general purpose queries and logic.'
+    #    )
+    #    self.tools.append(self.llm_tool)
+###################################
+        prompt = PromptTemplate(
+            input_variables=["query"],
+            template="{query}"
+        )
+
+        self.llm_chain = LLMChain(llm=self.llm, prompt=prompt)
+
+        # initialize the LLM tool
+        self.llm_tool = Tool(
+            name='Language Model',
+            func=self.llm_chain.run,
+            description='use this tool for general purpose queries and logic')
+        self.tools.append(self.llm_tool)
+            ########################
+
         # initialize the zero-shot agent
         self.zero_shot_agent = initialize_agent(
             agent="zero-shot-react-description",
@@ -41,3 +67,4 @@ class Agent:
     def get_agent_response(self, question):
         result = self.zero_shot_agent(question)
         return result
+
