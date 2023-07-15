9fa418eee8c86456d34d92081a99e67d3076c6fd
piotek8 <piotr.bator.contact@gmai.com>
2023-07-15 15:21:24 +0200

    it's done - agent ( LLM and Math questions). will add scraper to get informations from website and connect with vector db

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from core.agent import Agent
+from core.agent import Agent, Program
 import warnings
 
 # Ignore the specific warning
@@ -8,63 +8,20 @@ if __name__ == "__main__":
     # create an instance of Agent
     agent = Agent()
 
-'''
-    while True:
-        # prompt user for choice
-        choice = input("Enter:\n - '1' to use Calculator\n - '2' to use Language Model \n - 'exit' to quit \n"
-                       "What is your choice?")
+    # create an instance of Program
+    program = Program(agent)
 
-        if choice == '1':
-            calculation = input("Enter the question for the Calculator: ")
-            result = agent.get_agent_response(calculation)
-        elif choice == '2':
-            query = input("Enter the question for the Language Model: ")
-            result = agent.get_agent_response(query)
-        elif choice.lower() == 'exit':
-            break
-        else:
-            print("\nInvalid choice. Please enter 1, 2, or 'exit'.")
-            continue  # Go back to the beginning of the loop
+    # run the program
+    program.run()
 
-        # prompt user to continue or exit
-        continue_choice = input("Enter 1 or 2 to continue using the respective tool, or 'exit' to quit: ")
+# Calculator:
 
-        if continue_choice.lower() == 'exit':
-            break
-        elif continue_choice not in ['1', '2']:
-            print("Invalid choice. Please enter 1, 2, or 'exit'.")
-            break
-'''
+# What is (6.53*14.1)^6.22?
+# What is cosine of 90 degrees?
+# What is 15 raised to the power x? given x equals 1/5?
 
+# Language Model:
 
-
-
-
-
-
-
-
-
-
-
-
-#from core.agent import Agent
-#
-#if __name__ == "__main__":
-#    # create an instance of Agent
-#    agent = Agent()
-#
-#    # run the agent
-#    question = input('''Write informations \nEnter your question: ''')
-#    agent.get_agent_response(question)
-
-
-
-    # what is (6.53*14.1)^6.22?
-    # "if Mary has four apples and Giorgio brings two and a half apple "
-    #               "boxes (apple box contains eight apples), how many apples do we "
-    #               "have?")
-#The famr consists of a house,
-# a shack and a barn. How many dogs are on the farm?
-# If 2 dogs are in the kennel and 2 other dogs are standing next to the house.
-
+# In the backyard, there are 7 cows: one is white, the second one is black, and the rest are white-black in color. How many cows are white-black?
+# The farm consists of a house, a shack and a barn. How many dogs are on the farm? If 2 dogs are in the kennel and 2 other dogs are standing next to the house.
+# if Mary has four apples and Giorgio brings two and a half apple boxes (apple box contains eight apples), how many apples do we have?")
--- core/agent.py
+++ core/agent.py
@@ -7,39 +7,28 @@ from langchain.chains import LLMMathChain, LLMChain
 from langchain.chat_models import ChatOpenAI
 from langchain.prompts import PromptTemplate
 
-#from utils.constants import OPENAI_KEY
 
+# from utils.constants import OPENAI_KEY
 
 class Agent:
     def __init__(self):
         load_dotenv(find_dotenv())
         self.llm = ChatOpenAI(
             openai_api_key=os.getenv("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"),
-            #(OPENAI_KEY),
+            # (OPENAI_KEY),
             temperature=0,
             model_name="gpt-3.5-turbo-16k"
         )
 
         self.llm_math = LLMMathChain(llm=self.llm)
-        # initialize the math tool
         self.math_tool = Tool(
             name='Calculator',
             func=self.llm_math.run,
             description='Useful for when you need to answer questions about math.'
         )
 
-        # when giving tools to LLM, we must pass as a list of tools
         self.tools = [self.math_tool]
 
-    #    # initialize the LLM tool
-    #    self.llm_chain = LLMChain(llm=self.llm)
-    #    self.llm_tool = Tool(
-    #        name='Language Model',
-    #        func=self.llm_chain.run,
-    #        description='Use this tool for general purpose queries and logic.'
-    #    )
-    #    self.tools.append(self.llm_tool)
-###################################
         prompt = PromptTemplate(
             input_variables=["query"],
             template="{query}"
@@ -54,7 +43,6 @@ class Agent:
             description='use this tool for general purpose queries and logic')
         self.tools.append(self.llm_tool)
 
-
         # initialize the zero-shot agent
         self.zero_shot_agent = initialize_agent(
             agent="zero-shot-react-description",
@@ -68,3 +56,24 @@ class Agent:
         result = self.zero_shot_agent(question)
         return result
 
+
+class Program:
+    def __init__(self, agent):
+        self.agent = Agent()
+
+    def run(self):
+        while True:
+            choice = input("Enter:\n - '1' to use Calculator\n - '2' to use Language Model\n - 'exit' to quit\n"
+                           "What is your choice? ")
+
+            if choice == '1':
+                calculation = input("Enter the question for the Calculator: ")
+                result = self.agent.get_agent_response(calculation)
+            elif choice == '2':
+                query = input("Enter the question for the Language Model: ")
+                result = self.agent.get_agent_response(query)
+            elif choice.lower() == 'exit':
+                break
+            else:
+                print("\nInvalid choice. Please enter 1, 2, or 'exit'.")
+                continue
